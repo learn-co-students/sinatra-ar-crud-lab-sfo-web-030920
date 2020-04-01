223bb167c520ce48753d28c1d1e4e312f2f2e22b
lalogaribo <garibo.eduardo90@gmail.com>
2020-04-01 09:50:30 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,53 @@
-
-require_relative '../../config/environment'
+require_relative "../../config/environment"
 
 class ApplicationController < Sinatra::Base
-
   configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+    set :public_folder, "public"
+    set :views, "app/views"
   end
 
-  get '/' do
+  get "/" do
+  end
+
+  get "/articles" do
+    @articles = Article.all
+    erb :'articles/index'
+  end
+
+  get "/articles/new" do
+    erb :'articles/new'
+  end
+  get "/articles/:id" do
+    @article = Article.find(params[:id])
+    erb :'articles/show'
+  end
+
+  post "/articles" do
+    @article = Article.new(params)
+    if @article.valid?
+      @article.save
+      redirect "/articles/#{@article.id}"
+    else
+      erb :'articles/new', locals: { message: "Something went wrong with your article" }
+    end
+  end
+
+  get "/articles/:id/edit" do
+    @article = Article.find(params[:id])
+    erb :'articles/edit'
+  end
+
+  patch "/articles/:id" do
+    @article = Article.find(params[:id])
+    @article.title = params["title"]
+    @article.content = params["content"]
+    @article.save
+    redirect "/articles/#{@article.id}"
+  end
+
+  delete "/articles/:id" do
+    @article = Article.find(params[:id])
+    @article.destroy if @article
+    redirect "/articles"
   end
 end
